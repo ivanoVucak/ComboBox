0d5493e47423b2926dc1bc6cb520209d782e7c9c
ivanoVucak <145533812+ivanoVucak@users.noreply.github.com>
2023-10-04 08:27:37 +0200

    Add files via upload

---
+++ ComboBoxApp/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace ComboBoxApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.odaberiCb = new System.Windows.Forms.ComboBox();
+            this.odabranoTb = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // odaberiCb
+            // 
+            this.odaberiCb.FormattingEnabled = true;
+            this.odaberiCb.Items.AddRange(new object[] {
+            "IT menadzment",
+            "aplikativno programiranje",
+            "internet programiranje",
+            "informacioni sistemi"});
+            this.odaberiCb.Location = new System.Drawing.Point(35, 34);
+            this.odaberiCb.Name = "odaberiCb";
+            this.odaberiCb.Size = new System.Drawing.Size(148, 21);
+            this.odaberiCb.TabIndex = 0;
+            this.odaberiCb.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // odabranoTb
+            // 
+            this.odabranoTb.Location = new System.Drawing.Point(21, 82);
+            this.odabranoTb.Name = "odabranoTb";
+            this.odabranoTb.Size = new System.Drawing.Size(193, 20);
+            this.odabranoTb.TabIndex = 1;
+            this.odabranoTb.Text = "Odabrali ste: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 134);
+            this.Controls.Add(this.odabranoTb);
+            this.Controls.Add(this.odaberiCb);
+            this.Name = "Form1";
+            this.Text = "Upotreba ComboBox kontrole";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox odaberiCb;
+        private System.Windows.Forms.TextBox odabranoTb;
+    }
+}
+
---
+++ ComboBoxApp/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboBoxApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (odaberiCb.SelectedItem == "IT menadzment")
+            {
+                odabranoTb.Text = "Odabrali ste: IT menadzment";
+            }
+            if (odaberiCb.SelectedItem == "aplikativno programiranje")
+            {
+                odabranoTb.Text = "Odabrali ste: aplikativno programiranje";
+            }
+            if (odaberiCb.SelectedItem == "internet programiranje")
+            {
+                odabranoTb.Text = "Odabrali ste: internet programiranje";
+            }
+            if (odaberiCb.SelectedItem == "informacioni sistemi")
+            {
+                odabranoTb.Text = "Odabrali ste: informacioni sistemi";
+            }
+
+        }
+    }
+}
